2ac1bbebd06b45bbabd13c380d36eaba714a475c
Grace Lin <cheerfulkid74@gmail.com>
2024-01-11 00:27:29 +0800

    202401111225-建立登入/註冊頁基本html結構(第二步驟尚未完成)+新增與安裝手動router+修改.gitignore忽略檔案

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 import './output.css'
 
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
