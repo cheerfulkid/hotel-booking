81248ae2f331df78fe19ddb9121d9db9243ef662
Grace Lin <cheerfulkid74@gmail.com>
2024-01-11 00:08:14 +0800

    202401111206-建置登入/註冊頁結構(第二步驟尚未完成)+入口引入樣式更改為tailwind自產對應樣式

---
+++ src/view/login.vue
@@ -0,0 +1,36 @@
+<template>
+ <header class="bg-black py-2 px-5">
+  <img class="w-48" src="./assets/img/pc/logo.png" alt="享樂酒店">
+ </header>
+ <main>
+  <div class="d-flex">
+   <div class="col">A</div>
+   <div class="col">
+    <span>享樂酒店，誠摯歡迎</span>
+    <h3>立即開始旅程</h3>
+    <form>
+     <div>
+      <label for="">電子信箱</label>
+      <input type="text" placeholder="hello@exsample.com">
+     </div>
+     <div> 
+      <label for="">密碼</label>
+      <input type="text" placeholder="請輸入密碼">
+     </div>
+     <div>
+      <div>
+       <label for="">記住帳號</label>
+       <input type="text">
+      </div>
+      <a href="">忘記密碼？</a>
+      
+     </div>
+     <div>
+      <a href="">會員登入</a>
+     </div>
+    </form>
+    <p>沒有會員嗎？<a href="">前往註冊</a></p>
+   </div>
+  </div>
+ </main>
+</template>
---
+++ src/view/signup.vue
@@ -0,0 +1,42 @@
+<template>
+ <header class="bg-black py-2 px-5">
+  <img class="w-48" src="./assets/img/pc/logo.png" alt="享樂酒店">
+ </header>
+ <main>
+  <div class="d-flex">
+   <div class="col">A</div>
+   <div class="col">
+    <span>享樂酒店，誠摯歡迎</span>
+    <h3>立即註冊</h3>
+    <div>
+     <div>
+      <span>1</span>
+      <p>輸入信箱及密碼</p>
+     </div>
+     <div>
+      <span>2</span>
+      <p>填寫基本資料</p>
+     </div>
+    </div>
+    <form>
+     <div>
+      <label for="">電子信箱</label>
+      <input type="text" placeholder="hello@exsample.com">
+     </div>
+     <div> 
+      <label for="">密碼</label>
+      <input type="text" placeholder="請輸入密碼">
+     </div>
+     <div> 
+      <label for="">確認密碼</label>
+      <input type="text" placeholder="請再輸入一次密碼">
+     </div>     
+     <div>
+      <a href="">下一步</a>
+     </div>
+    </form>
+    <p>已經有會員了嗎？<a href="">立即登入</a></p>
+   </div>
+  </div>
+ </main>
+</template>
