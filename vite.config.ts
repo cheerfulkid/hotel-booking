a2f2a997e6821c843634ef12dd916e00aec45fc2
Grace Lin <cheerfulkid74@gmail.com>
2024-02-04 21:37:10 +0800

    202402042137-調整vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import vue from '@vitejs/plugin-vue'
@@ -5,6 +6,8 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production'
+  ? '/hotel-booking/' : '/',
   plugins: [
     vue(), 
     createSvgIconsPlugin({
@@ -13,4 +16,9 @@ export default defineConfig({
       customDomId: '__svg__icons__dom__',
     })
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
 })
